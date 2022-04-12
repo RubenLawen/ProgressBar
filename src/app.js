7d437b0185dc48dd95fe64747a252714a3c81d55
RubenLawen <rcrochet@edenschool.fr>
2022-04-12 11:22:54 +0200

    last commit the progessbar is finished

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
-const mainbar = document.querySelector('mainprogressbar');
-const widthbar = document.querySelector('progresswidth')
+const mainbar = document.querySelector(".mainprogressbar");
+const widthbar = document.querySelector(".progresswidth");
 
-let random = parseInt(Math.random() * (14 - 3) + 3)
+let random = parseInt(Math.random() * (14 - 3) + 3);
 let maxWidth = 797;
-console.log(random)
+let temp = 0;
+let add = maxWidth / random;
+let adder = add;
+
+console.log(random);
+widthbar.style.transition = `${random / 4}s linear`;
+
+const inter = setInterval(() => {
+  widthbar.style.width = `${adder}px`;
+  adder = adder + add;
+  console.log(parseInt(adder));
+  if (parseInt(adder) >= maxWidth) {
+    clearInterval(inter);
+    widthbar.style.width = `${maxWidth - 1}px`;
+  }
+}, 1000);
