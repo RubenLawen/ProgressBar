7d8abe73114fde2f1840ccb3f76ce8f056481f97
RubenLawen <rcrochet@edenschool.fr>
2022-04-12 10:10:34 +0200

    create the progress bar and the design color

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const mainbar = document.querySelector('mainprogressbar');
+const widthbar = document.querySelector('progresswidth')
+
+let random = parseInt(Math.random() * (14 - 3) + 3)
+let maxWidth = 797;
+console.log(random)
